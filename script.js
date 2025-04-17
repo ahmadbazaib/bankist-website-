a4f51143d80612b12a24c8ca0f1f017b5354fc9b
Ahmad <ahmadbazaib7@gmail.com>
2025-04-17 12:30:13 +0530

    events and event_handlers

--- script.js
+++ script.js
@@ -159,3 +159,20 @@ console.log(logo.dataset.numberVersion);
 // Don't do this
 // logo.className = 'ahmad';
 */
+
+//////////////////////////////////////////////
+// TYPES OF EVENTS AND EVENT HANDLER
+
+const h1 = document.querySelector('h1');
+const alertH1 = function (e) {
+  alert('addEventListener: Good! You reading the heading');
+
+  // h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1);
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 5000);
+
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Good! You reading the heading');
+// };
